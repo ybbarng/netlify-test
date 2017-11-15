b675bf3fe40e02422c2cafa2ef4d5be945058eef
ybbarng <ybbarng@gmail.com>
2017-11-15 18:41:05 +0900

    admin으로 연결되는 로그인 버튼 구현

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Menu from '../Menu';
 import './style.scss';
+import '../../assets/fonts/fontello-3d8dd48a/css/fontello.css';
 
 class Header extends React.Component {
   render() {
     const menu = this.props.data;
     return (
-      <div className="header">
-        <Menu data={menu} />
+      <div className="header-wrapper">
+        <div className="header">
+          <Menu data={menu} />
+          <a className="login" href="/admin/"><i className="icon-login" /></a>
+        </div>
       </div>
     );
   }
